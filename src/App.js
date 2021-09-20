2e900c91b2343310c88dabfe941eb80dadb2cdb1
Will Hoppin <whoppin@middlebury.edu>
2021-09-20 13:33:05 -0400

    landing page v1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <a href="https://middlebury.edu/" className="Nav" target="_blank" rel="noreferrer">
+        <img className="Logo" src="/middlogo.png" alt=""/>
+        <div className="Logo-text">
+          Middlebury
+        </div>
+      </a>
+      <div className="Masthead">
+        <div className="Left-element">
+          <div className="Main-title">
+            Study Groups
+          </div>
+          <div className="Main-subtitle">
+            Finally, place to ask all the <span className="Bold">"stupid"</span> questions
+          </div>
+          <div className="Buttons-wrapper">
+            <a className="Button Create-button">
+              Create a Group
+            </a>
+            <a className="Button Join-button">
+              Join a Group
+            </a>
+          </div>
+        </div>
+        <div className="Right-element">
+          <img className="Hero" src="/hero.jpg" alt=""/>
+        </div>
+      </div>
     </div>
   );
 }
